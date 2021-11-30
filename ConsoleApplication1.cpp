d63176a63aa9ba31b2405edc01e984f1a09cf94f
robin <robin@ROBINHOOD-LAPTO>
2021-11-30 11:43:37 -0500

    Add VendingMachine

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+int main() {
+	int x = 4;
+	int y = 2;
+
+	cout << "Результат умножения " << x << " и " << y << " равен " << x * y << endl;
+	return 0;
+
+}
